a32d62e35cdc0ec97e0a443ea615598cb3631acb
Gary Dix <intelproof@gmail.com>
2025-06-25 14:13:56 +1200

    finished!!!

--- Operations/DbOperations.js
+++ Operations/DbOperations.js
@@ -1,13 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
-
 import SQLite from "react-native-sqlite-storage";
-// import { SafeAreaView, ScrollView, Text, View, ImageBackground } from "react-native";
-// import { useDbOperationStyles } from "../AllStyles/dbOperationsStyles";
-// import { useAppStyles } from "../AllStyles/appStyles";
-// import { Context } from "./Context";
-// import DbButtons from "../Components/DbButtons";
-// import { Pressable, Alert } from "react-native";
-// import Dialog from "react-native-dialog";
 
 export const loadDB = () => {
   return SQLite.openDatabase(
